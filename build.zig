384b1502863977bdcdbd7af601e6070ebff34f15
ethanqm <q.ethan.moore@gmail.com>
2024-07-25 14:44:47 +1000

    understand build a bit more

--- build.zig
+++ build.zig
@@ -15,18 +15,18 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const bf_lib = b.addStaticLibrary(.{
-        .name = "bf",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/bf.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    //const bf_lib = b.addStaticLibrary(.{
+    //    .name = "bf",
+    //    // In this case the main source file is merely a path, however, in more
+    //    // complicated build scripts, this could be a generated file.
+    //    .root_source_file = b.path("src/bf.zig"),
+    //    .target = target,
+    //    .optimize = optimize,
+    //});
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
-    b.installArtifact(bf_lib);
+    // b.installArtifact(bf_lib);
 
     const exe = b.addExecutable(.{
         .name = "bfi",
@@ -34,6 +34,8 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+
+    //exe.linkLibrary(bf_lib);
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
